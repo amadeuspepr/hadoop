3dd4f468f926595e728e1c4731b30fcae663e5b7
Pierre-Etienne Melet <pmelet@amadeus.com>
2016-10-21 16:59:41 +0200

    add the possibility to push some data

--- start-yarn.sh
+++ start-yarn.sh
@@ -8,6 +8,15 @@ ssh-keyscan -H localhost 0.0.0.0 >> /root/.ssh/known_hosts
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
 
+sleep 1; 
+
+if [ -d /localfolder ]; then
+    echo "Copy content of /localfolder"
+    $HADOOP_PREFIX/bin/hdfs dfs -copyFromLocal /localfolder /localfolder
+else
+    echo "Nothing to copy: no folder named /localfolder"
+fi
+
 if [[ $1 == "-d" ]]; then
   while true; do 
   	sleep 1000; 
